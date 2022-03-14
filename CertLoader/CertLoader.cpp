38225c2744f1aca3d0988b788f288a5db5d60684
Jozsef Dojcsak <jozsef.dojcsak@gmail.com>
2022-03-14 11:38:25 +0100

    Add support for NCrypt based loader

--- CertLoader/CertLoader.cpp
+++ CertLoader/CertLoader.cpp
@@ -1,5 +1,6 @@
 #pragma comment (lib, "Winscard")
 #pragma comment (lib, "Crypt32")
+#pragma comment (lib, "Ncrypt.lib")
 
 #include <Windows.h>
 
@@ -21,7 +22,7 @@ using WString = std::wstring;
 using octet = uint8_t;
 using Blob = std::vector<octet>;
 
-static WString s2ws(const String s) {
+static WString s2ws(const String& s) {
 	size_t conv_count;
 	WString ws;
 	ws.resize(s.size() + 1);
@@ -262,12 +263,18 @@ struct ReaderStateMonitor {
 };
 
 struct Certificate {
-	PCCERT_CONTEXT context = nullptr;
-	Certificate(PCCERT_CONTEXT context) : context{ context } {}
-	Certificate(const Certificate& cert) : context { cert.context } {
-		//FIXME: duplicate context?
+	static Certificate parse(const Blob& certData) {
+		PCCERT_CONTEXT pCert = CertCreateCertificateContext(X509_ASN_ENCODING | PKCS_7_ASN_ENCODING, certData.data(), (DWORD)certData.size());
+		if (pCert == nullptr) {
+			throw Failure{ L"Failed to create certificate context" };
+		}
+
+		return Certificate{ pCert };
 	}
-	//FIXME: move contructor??
+
+	PCCERT_CONTEXT context = nullptr;
+	Certificate(PCCERT_CONTEXT context) noexcept : context{ context } {}
+	Certificate(const Certificate& cert) = delete; // no copy
 
 	virtual ~Certificate() {
 		if (context != nullptr) CertFreeCertificateContext(context);
@@ -292,63 +299,64 @@ struct Certificate {
 	}
 };
 
-struct CardKeyContainer {
-	HCRYPTPROV handle = 0;
+/// Legacy CryptoAPI based certificate loader
+struct ContainerContext {
+	struct Key {
+		HCRYPTPROV handle = 0;
 
-	CardKeyContainer(const WString& reader, const WString& name) {
-		WString readerContainer = L"\\\\.\\" + reader + L"\\" + name;
+		Key(const WString& reader, const WString& name) {
+			WString readerContainer = L"\\\\.\\" + reader + L"\\" + name;
 
-		BOOL fSts = CryptAcquireContext(&handle, readerContainer.c_str(), MS_SCARD_PROV, PROV_RSA_FULL, CRYPT_SILENT);
-		if (!fSts) {
-			throw Failure{ L"Failed to get key container context!" };
-		}
-	}
-	~CardKeyContainer() {
-		if (handle != 0) CryptReleaseContext(handle, 0);
-	}
-	Certificate get() {
-		HCRYPTKEY hKey = 0;
-		DWORD dwKeySpec = AT_KEYEXCHANGE;
-		DWORD dwError = 0;
-		if (!CryptGetUserKey(handle, dwKeySpec, &hKey)) {
-			dwError = GetLastError();
-			if (dwError == 0x8009000D) {
-				dwKeySpec = AT_SIGNATURE;
-				if (!CryptGetUserKey(handle, dwKeySpec, &hKey)) {
-					dwError = GetLastError();
-				}
+			BOOL fSts = CryptAcquireContext(&handle, readerContainer.c_str(), MS_SCARD_PROV, PROV_RSA_FULL, CRYPT_SILENT);
+			if (!fSts) {
+				throw Failure{ L"Failed to get key container context!" };
 			}
 		}
-		if (hKey == 0) {
-			throw Failure{ L"Failed to open key container", dwError };
+		~Key() {
+			if (handle != 0) CryptReleaseContext(handle, 0);
 		}
+		Certificate get() {
+			HCRYPTKEY hKey = 0;
+			DWORD dwKeySpec = AT_KEYEXCHANGE;
+			DWORD dwError = 0;
+			if (!CryptGetUserKey(handle, dwKeySpec, &hKey)) {
+				dwError = GetLastError();
+				// a wrong KeySpec may cause NTE_NO_KEY
+				if (dwError == NTE_NO_KEY) {
+					dwKeySpec = AT_SIGNATURE; // no support for EC
+					if (!CryptGetUserKey(handle, dwKeySpec, &hKey)) {
+						dwError = GetLastError();
+					}
+				}
+			}
+			if (hKey == 0) {
+				throw Failure{ L"Failed to open key container", dwError };
+			}
 
-		DWORD dwCertSize = 0;
-		PCCERT_CONTEXT pCert = nullptr;
-		if (CryptGetKeyParam(hKey, KP_CERTIFICATE, NULL, &dwCertSize, 0)) {
-			Blob certData(dwCertSize, 0x00);
-			if (CryptGetKeyParam(hKey, KP_CERTIFICATE, certData.data(), &dwCertSize, 0)) {
-				pCert = CertCreateCertificateContext(X509_ASN_ENCODING | PKCS_7_ASN_ENCODING, certData.data(), dwCertSize);
+			DWORD dwCertSize = 0;
+			Blob certData;
+			dwError = 0;
+			if (CryptGetKeyParam(hKey, KP_CERTIFICATE, NULL, &dwCertSize, 0)) {
+				certData.resize(dwCertSize);
+				if (!CryptGetKeyParam(hKey, KP_CERTIFICATE, certData.data(), &dwCertSize, 0)) {
+					dwError = GetLastError();
+				}
 			} else {
 				dwError = GetLastError();
 			}
-		} else {
-			dwError = GetLastError();
+			CryptDestroyKey(hKey);
+			if (dwError != 0) {
+				throw Failure{ L"Failed to get key certificate", dwError };
+			}
+			return Certificate::parse(certData);
 		}
-		CryptDestroyKey(hKey);
-		if (pCert == nullptr) {
-			throw Failure{ L"Failed to create certificate context", dwError };
-		}
+	};
 
-		return Certificate{ pCert };
-	}
-};
 
-struct CardContainer {
 	WString reader;
 	HCRYPTPROV handle = 0;
 
-	CardContainer(const WString& reader) : reader{ reader } {
+	ContainerContext(const WString& reader) : reader{ reader } {
 		WString readerContainer = L"\\\\.\\" + reader + L"\\";
 
 		BOOL fSts = CryptAcquireContext(&handle, readerContainer.c_str(), MS_SCARD_PROV, PROV_RSA_FULL, CRYPT_SILENT);
@@ -356,7 +364,7 @@ struct CardContainer {
 			throw Failure{ L"Failed to get default context!" };
 		}
 	}
-	~CardContainer() {
+	~ContainerContext() {
 		if (handle != 0) CryptReleaseContext(handle, 0);
 	}
 
@@ -370,7 +378,7 @@ struct CardContainer {
 		}
 
 		while (fSts) {
-			CardKeyContainer keyContainer{ reader, s2ws(szTemp) };
+			Key keyContainer{ reader, s2ws(szTemp) };
 			callback(keyContainer.get());
 			dwSize = sizeof(szTemp);
 			fSts = CryptGetProvParam(handle, PP_ENUMCONTAINERS, (BYTE*)szTemp, &dwSize, CRYPT_NEXT);
@@ -378,35 +386,135 @@ struct CardContainer {
 	}
 };
 
-int main() {
-	std::wcout << "Monitoring smart cards, press <ESC> to exit" << std::endl;
+
+/// NCrypt based certificate loader
+struct StorageProvider {
+	struct Key {
+		NCRYPT_KEY_HANDLE handle = 0;
+		WString name;
+
+		Key(StorageProvider& provider, const WString& name) : name(name) {
+			auto rc = NCryptOpenKey(provider.handle, &handle, name.c_str(), AT_KEYEXCHANGE, 0);
+			if (rc != ERROR_SUCCESS) {
+				throw Failure{ L"NCryptOpenKey failed", (DWORD)rc };
+			}
+		}
+		virtual ~Key() {
+			if (handle != 0) NCryptDeleteKey(handle, 0);
+		}
+
+		Certificate get() {
+			DWORD dwSize = 0;
+			auto rc = NCryptGetProperty(handle, NCRYPT_CERTIFICATE_PROPERTY, NULL, 0, &dwSize, 0);
+			if (rc != ERROR_SUCCESS) {
+				throw Failure{ L"NCryptGetProperty(NCRYPT_CERTIFICATE_PROPERTY) size failed", (DWORD)rc };
+			}
+			Blob certData(dwSize, 0);
+			rc = NCryptGetProperty(handle, NCRYPT_CERTIFICATE_PROPERTY, certData.data(), certData.size(), &dwSize, 0);
+			if (rc != ERROR_SUCCESS) {
+				Failure{ L"NCryptGetProperty(NCRYPT_CERTIFICATE_PROPERTY) failed", (DWORD)rc };
+			}
+			return Certificate::parse(certData);
+		}
+	};
+
+
+	NCRYPT_PROV_HANDLE handle = 0;
+	WString reader;
+
+	StorageProvider(const WString& name) : reader{ name } {
+		DWORD dwFlags = NCRYPT_SILENT_FLAG;
+		SECURITY_STATUS rc = NCryptOpenStorageProvider(&handle, MS_SMART_CARD_KEY_STORAGE_PROVIDER, dwFlags);
+		if (rc != ERROR_SUCCESS) {
+			throw Failure{ L"NCryptOpenStorageProvider failed", (DWORD)rc };
+		}
+		rc = NCryptSetProperty(handle, NCRYPT_READER_PROPERTY, (PBYTE)name.c_str(), (name.size() + 1) * sizeof(WString::value_type), dwFlags);
+		if (rc != ERROR_SUCCESS) {
+			NCryptFreeObject(handle);
+			throw Failure{ L"READER_PROPERTY cannot be set", (DWORD)rc };
+		}
+	}
+	virtual ~StorageProvider() {
+		if (handle != 0) NCryptFreeObject(handle);
+	}
+	void discover(std::function<void(const Certificate&)> callback) {
+		PVOID enumState = nullptr;
+		for (;;) {
+			NCryptKeyName* keyName = nullptr;
+			DWORD dwFlags = NCRYPT_SILENT_FLAG;
+			auto rc = NCryptEnumKeys(handle, NULL, &keyName, &enumState, dwFlags);
+			if (rc == NTE_NO_MORE_ITEMS) break;
+			if (rc != ERROR_SUCCESS) {
+				throw Failure{ L"NCryptEnumKeys failed", (DWORD)rc };
+			}
+
+			Key providerKey{ *this, keyName->pszName };
+			callback(providerKey.get());
+			NCryptFreeBuffer(keyName);
+		}
+	}
+};
+
+
+
+template<class T>
+struct CertLoader {
+	T loader;
+	CertLoader(const WString& reader) : loader{ reader } {}
+	void discover(std::function<void(const Certificate&)> callback) {
+		loader.discover(callback);
+	}
+};
+
+
+int main(int argc, char* argv[]) {
+	bool use_ncrypt = false;
+	if ( argc > 1 ) {
+		if (strcmp("-n", argv[1]) == 0) use_ncrypt = true;
+	}
+
+	std::cout << "Monitoring smart cards with ";
+	if (use_ncrypt) {
+		std::cout << "NCrypt";
+	} else {
+		std::cout << "Crypto API";
+	}
+	std::cout << ", press <ESC> to exit" << std::endl;
+
 	ReaderStateMonitor monitor;
 	bool run = true;
 	while (run) {
 		try {
-			monitor.peek([](auto& e) {
+			monitor.peek([use_ncrypt](auto& e) {
 				std::wcout << e.as_string() << std::endl;
 				switch (e.change) {
 				case StateChangeEvent::Change::CardInserted:
 					try {
-						CardContainer cardContainer{ e.reader };
 						std::wcout << L" ATR:" << blob_to_hex<WString>(e.atr) << std::endl;
-						cardContainer.discover([](auto& c) {
+						auto callback = [](const Certificate& c) {
 							std::wcout << L" Certificate" << std::endl
 								<< L" - subject: " << c.subject() << std::endl
 								<< L" - issuer: " << c.issuer() << std::endl
 								<< L" - serial: " << blob_to_hex<WString>(c.serial()) << std::endl;
-							});
+						};
+
+						if (use_ncrypt) {
+							CertLoader<StorageProvider> loader{ e.reader };
+							loader.discover(callback);
+						} else {
+							CertLoader<ContainerContext> loader{ e.reader };
+							loader.discover(callback);
+						}
 					}
 					catch (const Failure& f) {
-						std::wcout << L"Error accessing card container: " << f.error << L" - " << f.code << std::endl;
+						std::wcout << L"Error accessing card container: " << f.error << L" - 0x" << std::hex << f.code << std::endl;
 					}
 					break;
 				}
 				});
 		}
 		catch (const Failure& f) {
-			std::wcout << L"Error getting smart card status: " << f.error << L" - " << f.code << std::endl;
+			std::wcout << L"Error getting smart card status: " << f.error << L" - 0x" << std::hex << f.code << std::endl;
 		}
 
 		if (ReadKey() == VK_ESCAPE) {
@@ -418,4 +526,3 @@ int main() {
 	}
 	return 0;
 }
-
--- README.md
+++ README.md
@@ -3,7 +3,10 @@ Simple smart card monitor and certificate loader
 
 ## Concepts
 - Monitors smart card state changes with the PC/SC API
-- Loads certificates from the inserted cards with the legacy Crypto API
+- Loads certificates from the inserted cards either with the legacy Crypto API or with the NCrypt API
+
+## Command line options
+- Use the flag -n to activate NCrypt based loader
 
 ## Build
 Load the SLN file Visual Studio 2019 and hit build
